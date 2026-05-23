7b7f78f7e1d315c3e7fc850261acfacd6cc5f987
superricebug <superricebug@gmail.com>
2026-05-23 21:58:09 +0800

    Add files via upload

--- pwa.config.ts
+++ pwa.config.ts
@@ -6,8 +6,8 @@ const pwaOption: Partial<VitePWAOptions> = {
   includeAssets: ["icon.svg", "apple-touch-icon.png"],
   filename: "swx.js",
   manifest: {
-    name: "NewsNow",
-    short_name: "NewsNow",
+    name: "Godeyes",
+    short_name: "Godeyes",
     description: "Elegant reading of real-time and hottest news",
     theme_color: "#F14D42",
     icons: [
